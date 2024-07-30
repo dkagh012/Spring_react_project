ec77eccfc38ba6310c13bffcbc3dfa8e82dbfe92
dkagh012 <qowjdxo00@gmail.com>
2024-07-30 23:10:29 +0900

    11. 공통 객체 타입 선언

---
+++ board-back/src/main/java/com/jiraynor/board_back/DTO/object/BoardListItem.java
@@ -0,0 +1,21 @@
+package com.jiraynor.board_back.DTO.object;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardListItem {
+    private int boardNumber;
+    private String title;
+    private String content;
+    private String boardTitleImage;
+    private int favoriteCount;
+    private int commentCount;
+    private int viewCount;
+    private String writeDatetime;
+    private String writerNickname;
+    private String writerProfileImage;
+}

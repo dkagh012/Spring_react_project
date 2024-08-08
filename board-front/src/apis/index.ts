47bf5c5c6473e99bb96cd8bddb4cd3bdfda524b8
dkagh012 <qowjdxo00@gmail.com>
2024-08-09 00:16:49 +0900

    34. Auth 페이지 회원가입 API 연동

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { SignInRequestDto, SignUpRequestDto } from "./request/auth";
-import { SignInResponseDto } from "./response/auth";
+import { SignInResponseDto, SignUpResponseDto } from "./response/auth";
 import { ResponseDto } from "./response";
 
 const DOMAIN = "http://localhost:4000";
@@ -25,4 +25,17 @@ export const signInRequest = async (requestBody: SignInRequestDto) => {
   return result;
 };
 
-export const signUpRequest = async (requestBody: SignUpRequestDto) => {};
+export const signUpRequest = async (requestBody: SignUpRequestDto) => {
+  const result = await axios
+    .post(SIGN_UP_URL(), requestBody)
+    .then((response) => {
+      const responseBody: SignUpResponseDto = response.data;
+      return responseBody;
+    })
+    .catch((error) => {
+      if (!error.response.data) return null;
+      const responseBody: ResponseDto = error.response.data;
+      return responseBody;
+    });
+  return result;
+};
--- board-front/src/views/Authentication/index.tsx
+++ board-front/src/views/Authentication/index.tsx
@@ -1,9 +1,15 @@
-import React, { useState, KeyboardEvent, useRef, ChangeEvent } from "react";
+import React, {
+  useState,
+  KeyboardEvent,
+  useRef,
+  ChangeEvent,
+  useEffect,
+} from "react";
 import "./style.css";
 import InputBox from "components/InputBox";
-import { SignInRequestDto } from "apis/request/auth";
-import { signInRequest } from "apis";
-import { SignInResponseDto } from "apis/response/auth";
+import { SignInRequestDto, SignUpRequestDto } from "apis/request/auth";
+import { signInRequest, signUpRequest } from "apis";
+import { SignInResponseDto, SignUpResponseDto } from "apis/response/auth";
 import { ResponseDto } from "apis/response";
 import { useCookies } from "react-cookie";
 import { MAIN_PATH } from "constant";
@@ -203,7 +209,7 @@ export default function Authentication() {
     const addressDetailRef = useRef<HTMLInputElement | null>(null);
 
     // 페이지 번호 상태
-    const [page, setPage] = useState<1 | 2>(2);
+    const [page, setPage] = useState<1 | 2>(1);
 
     // 이메일 상태
     const [email, setEmail] = useState<string>("");
@@ -296,6 +302,36 @@ export default function Authentication() {
 
     // 다음 주소 검색 팝업 오픈 함수
     const open = useDaumPostcodePopup();
+
+    // sign up response 처리 함수
+    const signUpResponse = (
+      responseBody: SignUpResponseDto | ResponseDto | null
+    ) => {
+      if (!responseBody) {
+        alert("네트워크 이상입니다.");
+        return;
+      }
+      const { code } = responseBody;
+      if (code === "DE") {
+        setEmailError(true);
+        setEmailErrorMessage("중복되는 이메일 주소입니다.");
+      }
+      if (code === "DN") {
+        setNicknameError(true);
+        setNicknameErrorMessage("중복되는 닉네임입니다.");
+      }
+      if (code === "DT") {
+        setTelNumberError(true);
+        setTelNumberErrorMessage("중복되는 핸드폰 번호입니다.");
+      }
+      if (code === "VF") alert("모든 값을 입력하세요.");
+      if (code === "DBE") alert("데이터베이스 오류입니다.");
+
+      if (code !== "SU") return;
+
+      setView("sign-in");
+    };
+
     // event handler:  이메일 변경 이벤트 처리
     const onEmailChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
       const { value } = event.target;
@@ -370,7 +406,7 @@ export default function Authentication() {
 
     // event handler:  패스워드 확인 버튼 클릭 이벤트 처리
     const onPasswordCheckButtonClickHandler = () => {
-      if (passwordButtonIcon === "eye-light-off-icon") {
+      if (passwordCheckButtonIcon === "eye-light-off-icon") {
         setPasswordCheckButtonIcon("eye-light-on-icon");
         setPasswordCheckType("text");
       } else {
@@ -405,9 +441,7 @@ export default function Authentication() {
       event: KeyboardEvent<HTMLInputElement>
     ) => {
       if (event.key !== "Enter") return;
-      if (!nicknameRef.current) return;
       onNextButtonClickHandler();
-      nicknameRef.current.focus();
     };
 
     // event handler: 닉네임 키 다운 이벤트 처리
@@ -471,7 +505,64 @@ export default function Authentication() {
 
     // event handler : 회원가입 버튼 클릭 이벤트 처리
     const onSignUpButtonClickHandler = () => {
-      alert("회원가입 버튼 ~");
+      const emailPattern = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}$/;
+      const isEmailPattern = emailPattern.test(email);
+      if (!isEmailPattern) {
+        setEmailError(true);
+        setEmailErrorMessage("이메일 주소 포맷이 맞지 않습니다.");
+      }
+
+      const isCheckedPassword = password.trim().length >= 8;
+      if (!isCheckedPassword) {
+        setPasswordError(true);
+        setPasswordErrorMessage("비밀번호는 8자 이상 입력해주세요.");
+      }
+
+      const isEqualPassword = password === passwordCheck;
+      if (!isEqualPassword) {
+        setPasswordCheckError(true);
+        setPasswordCheckErrorMessage("비밀번호가 일치하지 않습니다.");
+      }
+
+      if (!isEmailPattern || !isCheckedPassword || !isEqualPassword) {
+        setPage(1);
+        return;
+      }
+
+      const hasNickname = nickname.trim().length !== 0;
+      if (!hasNickname) {
+        setNicknameError(true);
+        setNicknameErrorMessage("닉네임을 입력해주세요.");
+      }
+
+      const telNumberPattern = /^[0-9]{11,13}$/;
+      const isTelNumberPattern = telNumberPattern.test(telNumber);
+      if (!isTelNumberPattern) {
+        setTelNumberError(true);
+        setTelNumberErrorMessage("숫지만 입력해주세요.");
+      }
+
+      const hasAddress = address.trim().length > 0;
+      if (!hasAddress) {
+        setAddressError(true);
+        setAddressErrorMessage("주소를 선택해주세요.");
+      }
+
+      if (!agreedPersonal) setAgreedPersonalError(true);
+
+      if (!hasNickname || !isTelNumberPattern || !agreedPersonal) return;
+
+      const requestBody: SignUpRequestDto = {
+        email,
+        password,
+        nickname,
+        telNumber,
+        address,
+        addressDetail,
+        agreedPersonal,
+      };
+
+      signUpRequest(requestBody).then(signUpResponse);
     };
 
     // event handler : 로그인 링크 클릭 이벤트 처리
@@ -482,9 +573,18 @@ export default function Authentication() {
     const onComplete = (data: Address) => {
       const { address } = data;
       setAddress(address);
+      setAddressError(false);
+      setAddressErrorMessage("");
       if (!addressDetailRef.current) return;
       addressDetailRef.current.focus();
     };
+    // 페이지가 변경될 떄 마다 실행될 함수
+    useEffect(() => {
+      if (page === 2) {
+        if (!nicknameRef.current) return;
+        nicknameRef.current.focus();
+      }
+    }, [page]);
 
     // sign up card 컴포넌트 렌더링
     return (
